88d3a5a23b36cbcefff86cb42f53b9a0a745028d
Gareth Badenhorst <gareth.badenhorst@gmail.com>
2018-06-06 22:32:03 +0100

    First commit

---
+++ src/main/kotlin/com/plasmadrive/restfulwebservices/RestfulWebServicesApplication.kt
@@ -0,0 +1,11 @@
+package com.plasmadrive.restfulwebservices
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestfulWebServicesApplication
+
+fun main(args: Array<String>) {
+    runApplication<RestfulWebServicesApplication>(*args)
+}
